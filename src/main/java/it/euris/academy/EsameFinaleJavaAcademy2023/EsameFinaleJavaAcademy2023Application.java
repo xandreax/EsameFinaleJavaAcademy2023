3cc9cc3b33d951d55dd53a75738a76525220dda8
xandreax <xandreaxbora@gmail.com>
2023-03-20 09:20:18 +0100

    Initial commit

---
+++ src/main/java/it/euris/academy/EsameFinaleJavaAcademy2023/EsameFinaleJavaAcademy2023Application.java
@@ -0,0 +1,13 @@
+package it.euris.academy.EsameFinaleJavaAcademy2023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsameFinaleJavaAcademy2023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsameFinaleJavaAcademy2023Application.class, args);
+	}
+
+}
